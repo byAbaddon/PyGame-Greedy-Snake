524a0e6c3e4b3480791c18035f3b824fd7468610
byAbaddon <kasiopess@abv.bg>
2022-12-07 20:27:33 +0200

    add levels

--- src/classes/figure.py
+++ src/classes/figure.py
@@ -2,12 +2,15 @@ from src.settings import *
 
 
 class Figure(pygame.sprite.Sprite):
-
     levels_dict = {
-                   1: (0, 0),
-                   2: (S_W // 2 - BLOCK_SIZE * 2 + 10, BLOCK_SIZE + 15),
-                   3: (S_W // 2 - BLOCK_SIZE * 4 + 10, S_H // 3 - 17),
-                   }
+        1: (0, 0),
+        2: (S_W // 2 - BLOCK_SIZE * 2 + 10, BLOCK_SIZE + 15),
+        3: (S_W // 2 - BLOCK_SIZE * 4 - 20, S_H // 3 - 17 - BLOCK_SIZE * 2),
+        4: (S_W // 2 - 2, 196),
+        5: (S_W // 2, 194),
+        6: (S_W // 2, 194),
+
+    }
 
     def __init__(self, image):
         pygame.sprite.Sprite.__init__(self)
